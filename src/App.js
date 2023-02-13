35e66046a2fe56f2e55ac512db85222ef34628f9
JazzBlaze <thejasdub@gmail.com>
2023-02-14 02:48:05 +0530

    added gradient headiengs

--- src/App.js
+++ src/App.js
@@ -92,15 +92,15 @@ export default function App() {
               </div>
 
               <div className="hero-main">
-                <h1 id="elem" className="hero-main-title gradient-headings">VIBRANCE 2023</h1>
-                <p className="hero-main-description">Engage.Enthral.Entertain</p>
+                <h1 id="grad1" className="hero-main-title gradient-headings">VIBRANCE 2023</h1>
+                <p className="hero-main-description">Engage. Enthral. Entertain</p>
               </div>
 
 
 
               <div className="hero-second">
-                <p className="hero-second-subheading first-sub">Dive In</p>
-                <p className="hero-second-subheading second-sub">Deep</p>
+                <p id="grad2" className="hero-second-subheading first-sub gradient-headings">Dive In</p>
+                <p id="grad3" className="hero-second-subheading second-sub gradient-headings">Deep</p>
 
                 <div className="countdown">
                   <ul>
@@ -382,89 +382,4 @@ export default function App() {
   );
   
 }
-
-// function Model({ url, ...props }) {
-//   const { scene, animations } = useGLTF(url);
-//   const can=document.getElementById('can')
-//   can.style.background = "#EABFFF";
-//   scene.scale.set(0.4,0.4,0.4);
-//   scene.rotateX(-0.0872665);
-//   scene.position.set(0,-2,0);
-
-//   const actions = useAnimations(animations)
-
-  
-
-//   let mixer = new THREE.AnimationMixer(scene);
-//   animations.forEach((clip) => {
-//     const action = mixer.clipAction(clip);
-//     action.play();
-//   });
-
-//   console.log(animations);
-
-//   mixer.timeScale = 0.5;
-//   useFrame((state, delta) => {
-//     mixer.update(delta);
-//     mixer.setTime(99.9);
-//   });
-
-//   var lerp = {
-//     current: 0,
-//     target: 0,
-//     ease: 0.1,
-//   };
-//   window.addEventListener("mousemove", (e) => {
-//     var rotation =
-//         ((e.clientX - window.innerWidth / 2) * 2) / window.innerWidth;
-//     lerp.target = rotation * 0.05;
-
-//   });
-  
-//   useFrame( () => {
-//     lerp.current = gsap.utils.interpolate(
-//       lerp.current,
-//       lerp.target,
-//       lerp.ease
-//     );
-
-  
-//     scene.rotation.y= lerp.current;
-
-//   })
- 
-//   // function animate(){
-    
-//   //   requestAnimationFrame(animate);
-//   //   // mixer.update(clock.getDelta());
-//   //   // mixer.setTime(scrollPoint);
-//   // }
-  
-//   // animate();
-
-
-//   //preloader
-//   // var sizes= new Sizes();
-//   // const dev= sizes.device;
-//   // sizes.on("switchdevice", (device) => {
-//   //   dev =device;
-//   // });
-
-
-//   // convert(document.querySelector(".intro-text"));
-//   // convert(document.querySelector(".hero-main-title"));
-//   // convert(document.querySelectorAll(".hero-main-description"));
-//   // convert(document.querySelectorAll(".hero-second-subheading"));
-//   // convert(document.querySelector(".second-sub"));
-  
-
-
-
-
-
-  
-
-
-//   return <primitive object={scene} {...props} />;
-// }
 
--- src/Test.js
+++
@@ -1,9 +0,0 @@
-// import Experience from "./components/Experience";
-
-
-
-// export default function Test() {
-//     console.log("sd");
-//     console.log(document.getElementById("can"))
-
-// }
