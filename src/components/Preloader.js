cce69ad8d4c44ab43b080530e7e0981a468061bc
Steve Aldrin <stevealdrin.r2021@vitstudent.ac.in>
2023-02-14 23:21:34 +0530

    Update Sections

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -236,7 +236,7 @@ export default class Preloader extends EventEmitter {
 
     async playIntro() {
         this.scaleFlag = true;
-        await this.firstIntro();
+       await this.firstIntro();
         this.moveFlag = true;
         console.log(this.moveFlag);
         this.scrollOnceEvent = this.onScroll.bind(this);
